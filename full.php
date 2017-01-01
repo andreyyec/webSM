3ff45e8b34c2f0e6a39fed177dfc16b74752e9b1
Andrey Castro <a.castro.carvajal@accenture.com>
2016-12-31 18:59:33 -0600

    [First Commit] template setup
    Concrete5 webSM template

---
+++ full.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>This is a test</title>
+	<link rel="stylesheet" type="text/css" href="<?=$view->getThemePath();?>/css/test.css">
+</head>
+<body>
+This is a test
+</body>
+</html>
